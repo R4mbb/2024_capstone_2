7c7e1ead7b68f729ce863fdac201fba091a82384
jinsu9758 <jinsu9758@gmail.com>
2024-11-03 19:56:51 +0900

    remove soft.zip, modify lambda_function.py : tmp/check >> /tmp/check

--- lambda_function.py
+++ lambda_function.py
@@ -11,14 +11,14 @@ def s3_download_file():
     s3 = boto3.client('s3')
     bucket_name = 'pre-upload-amoin-bucket'
 
-    if not os.path.exists('tmp/check'):
-        os.makedirs('tmp/check')
+    if not os.path.exists('/tmp/check'):
+        os.makedirs('/tmp/check')
 
     response = s3.list_objects_v2(Bucket=bucket_name)
 
     for obj in response['Contents']:
         file_key = obj['Key']
-        download_path = f"tmp/check/" + file_key  # 파일명을 임시 경로로 지>정
+        download_path = f"/tmp/check/" + file_key  # 파일명을 임시 경로로 지>정
 
         # 파일 다운로드
         s3.download_file(bucket_name, file_key, download_path)
@@ -31,8 +31,8 @@ def s3_download_file():
 
 def handler(event, context):
     file = s3_download_file()
-    if os.path.exists('tmp/extracted'):
-        shutil.rmtree('tmp/extracted/')
+    if os.path.exists('/tmp/extracted'):
+        shutil.rmtree('/tmp/extracted/')
         
     pred = Lamb()
     result = pred.predict(file)
@@ -46,9 +46,8 @@ def handler(event, context):
     
     print(result, res.status_code)
 
-    if os.path.exists('tmp/check'):
-        shutil.rmtree('tmp/check')
-        os.makedirs('tmp/check')
+    if os.path.exists('/tmp/check'):
+        shutil.rmtree('/tmp/check')
 
 
 
